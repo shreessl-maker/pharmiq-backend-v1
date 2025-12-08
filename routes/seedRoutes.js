61b2edb6494efce92b30ee83154b32ee2185bd04
shreessl-maker <shreessl@gmail.com>
2025-12-08 15:04:45 +0530

    Create seedRoutes.js

---
+++ routes/seedRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { seedDemoData } from "../data/seedController.js";
+
+const router = express.Router();
+
+router.get("/run", seedDemoData);
+
+export default router;
