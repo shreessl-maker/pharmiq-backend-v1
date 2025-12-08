2a9ddc20c965d99d98e4ca9d8b0d1fbfb3cd930c
shreessl-maker <shreessl@gmail.com>
2025-12-08 10:24:04 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["admin", "manager", "staff"], default: "staff" },
+    clientId: { type: mongoose.Schema.Types.ObjectId, ref: "Client" },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
